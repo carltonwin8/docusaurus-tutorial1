740be761335165f9dcbb833372605e82f3df4a8c
Carlton Joseph <carlton.joseph@gmail.com>
2019-07-30 16:10:00 -0700

    fix typos

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: "Test Site", // Title for your website.
   tagline: "A website for testing",
   url: "carltonwin8.github.io", // Your website URL
-  baseUrl: "/docusaurus-tutorial1", // Base URL for your project */
+  baseUrl: "/docusaurus-tutorial1/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "docusaurus-classroom",
+  projectName: "docusaurus-tutorial1",
   organizationName: "carltonwin8",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
